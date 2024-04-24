5b37e8e10798f3eb414a3b17bffa6393b75cacb5
Alexander <alexander@balian.am>
2024-04-24 23:54:28 +0400

    2 zones

--- provider.tf
+++ provider.tf
@@ -2,10 +2,10 @@ locals {
   region          = "us-west-1"
   name            = "projet-cluster"
   vpc_cidr        = "10.20.0.0/16"
-  azs             = ["us-west-1a", "us-west-1b", "us-west-1c"]
-  public_subnets  = ["10.20.10.0/24", "10.20.12.0/24", "10.20.14.0/24"]
-  private_subnets = ["10.20.20.0/24", "10.20.22.0/24", "10.20.24.0/24"]
-  intra_subnets   = ["10.20.30.0/24", "10.20.32.0/24", "10.20.34.0/24"]
+  azs             = ["us-west-1b", "us-west-1c"]
+  public_subnets  = ["10.20.10.0/24", "10.20.12.0/24"]
+  private_subnets = ["10.20.20.0/24", "10.20.22.0/24"]
+  intra_subnets   = ["10.20.30.0/24", "10.20.32.0/24"]
   tags = {
     Example = local.name
   }
