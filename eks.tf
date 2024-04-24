aa5322b590b6b323d80f16cb13fef40065712a25
Alexander <alexander@balian.am>
2024-04-24 23:21:01 +0400

    fix

--- eks.tf
+++ eks.tf
@@ -18,7 +18,7 @@ module "eks" {
 
   vpc_id                   = module.vpc.vpc_id
   subnet_ids               = module.vpc.private_subnets
-  control_plane_subnet_ids = module.vpc.intra_subnet
+  control_plane_subnet_ids = module.vpc.intra_subnets
 
   eks_managed_node_group_defaults = {
     ami_type      = "AL2_x86_64"
