17b9e8393fc2464c18ba43a112f5b0b09e8787df
Alexander <alexander@balian.am>
2024-04-25 00:29:24 +0400

    ip

--- provider.tf
+++ provider.tf
@@ -3,9 +3,9 @@ locals {
   name            = "projet-cluster"
   vpc_cidr        = "10.20.0.0/16"
   azs             = ["us-west-1b", "us-west-1c"]
-  public_subnets  = ["10.20.10.0/24", "10.20.12.0/24"]
-  private_subnets = ["10.20.20.0/24", "10.20.22.0/24"]
-  intra_subnets   = ["10.20.30.0/24", "10.20.32.0/24"]
+  public_subnets  = ["10.20.10.0/24", "10.20.20.0/24"]
+  private_subnets = ["10.20.30.0/24", "10.20.40.0/24"]
+  intra_subnets   = ["10.20.50.0/24", "10.20.60.0/24"]
   tags = {
     Example = local.name
   }
