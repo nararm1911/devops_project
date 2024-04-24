4cae5ea6ae98a21f0b7052b4be21ca97fbe9686c
Alexander <alexander@balian.am>
2024-04-24 23:00:39 +0400

    commit

--- eks.tf
+++ eks.tf
@@ -7,28 +7,28 @@ module "eks" {
 
   cluster_addons = {
     coredns = {
-        most_recent = true
+      most_recent = true
     }
     kibe-proxy = {
-        most_recent = true
+      most_recent = true
     }
     vpc-cni = {
-        most_recent = true
-    }    
+      most_recent = true
+    }
   }
 
-  vpc_id = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
+  vpc_id                   = module.vpc.vpc_id
+  subnet_ids               = module.vpc.private_subnets
   control_plane_subnet_ids = module.vpc.intra_subnet
 
   eks_managed_node_group_defaults = {
-    ami_type="AL2_x86_64"
-    instance_type=["t2.micro"]
+    ami_type      = "AL2_x86_64"
+    instance_type = ["t2.micro"]
 
-    attach_cluster_primary_security_group=true
+    attach_cluster_primary_security_group = true
   }
-  
-  
+
+
   eks_managed_node_groups = {
     project-cluster-wg = {
       min_size     = 1
