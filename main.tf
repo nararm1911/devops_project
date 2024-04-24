a558e28e47cd8a8a20d2ead966549d4a3a343d4a
Alexander <alexander@balian.am>
2024-04-24 20:09:15 +0400

    main.tf

---
+++ main.tf
@@ -0,0 +1,9 @@
+provider "aws" {
+  region = "us-west-1"
+}
+
+resource "aws_instance" "Jen_terra_Ec2" {
+  ami           = "ami-05c969369880fa2c2"
+  instance_type = "t2.micro"
+  tags          = { Name = "Jen_terra_Ec2" }
+}
